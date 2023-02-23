9596a8282da1f9795855751880ae23e5263c1043
Peter Downs <github@peterdowns.com>
2023-02-23 18:30:53 -0500

    Get proxying working

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -1,53 +1,107 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig"             // config
 	_ "github.com/caddyserver/caddy/v2/caddyconfig/caddyfile" // caddy?
-	_ "github.com/caddyserver/caddy/v2/modules/standard"      // ahhhhhhhh?
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp/reverseproxy"
+	"github.com/caddyserver/caddy/v2/modules/caddytls"
+	_ "github.com/caddyserver/caddy/v2/modules/standard" // ahhhhhhhh?
 
 	"github.com/spf13/cobra"
 )
 
 func daemonImpl(_ *cobra.Command, _ []string) error {
-	// run the initial config
-	cfgAdapter := caddyconfig.GetAdapter("caddyfile")
-	if cfgAdapter == nil {
-		panic("whtf")
-	}
-	configContents := strings.TrimSpace(`
-local.test
+	// potentially: build config as caddyfile, much simler?
+	// https://caddyserver.com/docs/caddyfile/directives/tls#internal
 
-reverse_proxy :9000
-`)
-	config, warnings, err := cfgAdapter.Adapt([]byte(configContents), map[string]any{
-		"filename": "Caddyfile",
-	})
+	// proceed to build the handler and server
+	// based on
+	// https://github.com/caddyserver/caddy/blob/5ded580444e9258cb35a9c94192d3c1d63e7b74f/modules/caddyhttp/reverseproxy/command.go#L122
+	ht := reverseproxy.HTTPTransport{}
+	toAddresses := []string{":9000"}
+	upstreamPool := reverseproxy.UpstreamPool{}
+	for _, toAddr := range toAddresses {
+		upstreamPool = append(upstreamPool, &reverseproxy.Upstream{
+			Dial: toAddr,
+		})
+	}
+	handler := reverseproxy.Handler{
+		TransportRaw: caddyconfig.JSONModuleObject(ht, "protocol", "http", nil),
+		Upstreams:    upstreamPool,
+	}
+	route := caddyhttp.Route{
+		HandlersRaw: []json.RawMessage{
+			caddyconfig.JSONModuleObject(handler, "handler", "reverse_proxy", nil),
+		},
+	}
+	fromAddr, err := httpcaddyfile.ParseAddress("https://local.test")
 	if err != nil {
 		return err
 	}
-	for _, w := range warnings {
-		fmt.Printf("warning; %+v\n", w)
+	fromAddr.Host = "local.test"
+	fromAddr.Scheme = "https"
+	fromAddr.Port = "443"
+	if fromAddr.Host != "" {
+		route.MatcherSetsRaw = []caddy.ModuleMap{
+			{
+				"host": caddyconfig.JSON(caddyhttp.MatchHost{fromAddr.Host}, nil),
+			},
+		}
 	}
-	// config := &caddy.Config{}
-	// tlsApp := caddytls.TLS{
-	// 	Automation: &caddytls.AutomationConfig{
-	// 		Policies: []*caddytls.AutomationPolicy{{
-	// 			Subjects:   []string{fromAddr.Host},
-	// 			IssuersRaw: []json.RawMessage{json.RawMessage(`{"module":"internal"}`)},
-	// 		}},
-	// 	},
-	// }
-	// appsRaw["tls"] = caddyconfig.JSON(tlsApp, nil)
-
-	if err := caddy.Load(config, true); err != nil {
+	server := &caddyhttp.Server{
+		Routes: caddyhttp.RouteList{route},
+		Listen: []string{":" + fromAddr.Port},
+	}
+	// server.AutoHTTPS = &caddyhttp.AutoHTTPSConfig{DisableRedir: true}
+	httpApp := caddyhttp.App{
+		Servers: map[string]*caddyhttp.Server{"proxy": server},
+	}
+	appsRaw := caddy.ModuleMap{
+		"http": caddyconfig.JSON(httpApp, nil),
+	}
+	tlsApp := caddytls.TLS{
+		Automation: &caddytls.AutomationConfig{
+			Policies: []*caddytls.AutomationPolicy{{
+				Subjects:   []string{fromAddr.Host},
+				IssuersRaw: []json.RawMessage{json.RawMessage(`{"module":"internal"}`)},
+			}},
+		},
+	}
+	appsRaw["tls"] = caddyconfig.JSON(tlsApp, nil)
+	var false bool
+	cfg := &caddy.Config{
+		Admin: &caddy.AdminConfig{
+			Disabled: true,
+			Config: &caddy.ConfigSettings{
+				Persist: &false,
+			},
+		},
+		AppsRaw: appsRaw,
+	}
+	cfg.Logging = &caddy.Logging{
+		Logs: map[string]*caddy.CustomLog{
+			"default": {Level: "DEBUG"},
+		},
+	}
+	err = caddy.Run(cfg)
+	if err != nil {
 		return err
 	}
-	// time.Sleep(30 * time.Second)
-	return nil
+
+	for _, to := range toAddresses {
+		fmt.Printf("Caddy proxying %s -> %s\n", fromAddr.String(), to)
+	}
+	if len(toAddresses) > 1 {
+		fmt.Println("Load balancing policy: random")
+	}
+
+	select {}
 }
 
 var daemonCmd = &cobra.Command{ //nolint:gochecknoglobals
