86d3e5ed185231355bbf22393e182fedf639c096
Peter Downs <github@peterdowns.com>
2024-10-21 16:21:22 -0400

    feat: add caddyfile debug command

--- cmd/localias/debug/config.go
+++ cmd/localias/debug/config.go
@@ -13,6 +13,13 @@ var configFlags struct { //nolint:gochecknoglobals
 	Print *bool
 }
 
+func caddyImpl(_ *cobra.Command, _ []string) error {
+	cfg := shared.Config()
+	caddy := cfg.Caddyfile()
+	fmt.Println(caddy)
+	return nil
+}
+
 func configImpl(_ *cobra.Command, _ []string) error {
 	cfg := shared.Config()
 	if *configFlags.Print {
@@ -33,7 +40,14 @@ var configCmd = &cobra.Command{ //nolint:gochecknoglobals
 	RunE:  configImpl,
 }
 
+var caddyCmd = &cobra.Command{
+	Use:   "caddyfile",
+	Short: "show the Caddy configuration file used by localias",
+	RunE:  caddyImpl,
+}
+
 func init() {
 	configFlags.Print = configCmd.Flags().BoolP("print", "p", false, "print the contents of the config file")
 	Command.AddCommand(configCmd)
+	Command.AddCommand(caddyCmd)
 }
